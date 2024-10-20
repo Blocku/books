84554cfc4291d3d6bb0b7ce6e880d37cb0f59af7
block <urent111@ayandex.ru>
2024-10-20 18:09:49 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Header from "@/components/header";
+import Menu from "@/components/menu";
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Header/>
+      <Menu/>
+    </>
   );
 }
---
+++ src/components/menu.tsx
@@ -0,0 +1,7 @@
+export default function Menu(){
+    return(
+        <div className="hidden bg-[#222222]" >
+            HUI
+        </div>
+    )
+}
